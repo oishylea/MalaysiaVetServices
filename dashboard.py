84e66e5590e126134ddc11fddbbb96b3e8fdef8f
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-09-05 21:19:12 +0800

    dashboard

---
+++ dashboard.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Malaysia Vet Services", page_icon=":bar_chart:", layout="wide")
+
+st.title(":bar_chart: Malaysia Vet Services")
+st.markdown('<style>div.block-container{padding-top:2rem}</style>', unsafe_allow_html=True)
